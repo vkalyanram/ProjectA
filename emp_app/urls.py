eec9f5034ffd9546b7174941132f7219520fac10
kalyan <kalyan.2405@gmail.com>
2022-08-08 21:24:11 +0530

    added role and dep values

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
         path('edit_emp_details/<int:emp_id>',views.edit_emp_details,name='edit_emp_details'),
         path('edit_emp/<int:emp_id>',views.edit_emp,name='edit_emp'),
         path('del_emp/<int:emp_id>',views.del_emp,name='del_emp'),
-        path('search_emp',views.search_emp,name='search_emp'),
+        #path('search_emp',views.search_emp,name='search_emp'),
+        path('search',views.search,name='search'),
 
 ]
